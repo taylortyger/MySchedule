dfe3f0e619c9f201cdd2db6db0ed35b6db0c1314
Taylor Tyger <taylortyger@gmail.com>
2019-10-08 08:21:44 -0600

    Set up node server to serve static files. Also add one route which simply serves a static file.

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use('/', express.static('static'));
+
+//Another way to server out files via routing
+app.get('/schedule', (req,res) => {
+    res.sendFile('myschedule.html', {root: './static'});
+});
+
 app.listen(PORT, () => {
     console.log("Server is running on " + PORT + " port");
 });
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>');
-});
 
 
